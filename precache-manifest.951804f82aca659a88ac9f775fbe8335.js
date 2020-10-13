0dd76b5aa11bac4785cf0dc9bb7e33f5f93f3585
Jacques Gudath Uys <jaxuys@gmail.com>
2020-10-13 19:18:35 +0200

    deploy

---
+++ precache-manifest.951804f82aca659a88ac9f775fbe8335.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be77ae587da8bee1097",
+    "url": "/jacquesuys/css/app.bd70d789.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/jacquesuys/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1d95071801d7f70f6b40ba3166c3c0a7",
+    "url": "/jacquesuys/index.html"
+  },
+  {
+    "revision": "6a03d2b00d583f6a60eb",
+    "url": "/jacquesuys/js/about.c787ca3d.js"
+  },
+  {
+    "revision": "6be77ae587da8bee1097",
+    "url": "/jacquesuys/js/app.1bc184e6.js"
+  },
+  {
+    "revision": "9b1dc298592056006ccb",
+    "url": "/jacquesuys/js/chunk-vendors.c82f361d.js"
+  },
+  {
+    "revision": "a7b985238384dc336f0a16d577f68212",
+    "url": "/jacquesuys/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jacquesuys/robots.txt"
+  }
+]);
